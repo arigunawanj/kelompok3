650904e08ee948b2791e6a2fcaf636d3b4be1e96
arigunawanj <arigunawanjatmiko@gmail.com>
2022-09-29 13:51:43 +0700

    rombak menu admin

--- app/home.php
+++ app/home.php
@@ -99,7 +99,40 @@
     <div class="home-content">
       <i class='bx bx-menu' ></i>
     </div>
+    
+    <div class="container text-center">
+        <div class="row justify-content-start">
+            <div class="col">
+            <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+            </tbody>
+            </table>
+            </div>
+        </div>
+    </div>
   </section>
+
+  
 
   
   <script src="asset/script.js"></script>
